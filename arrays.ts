f16d8f6c98a83909776ed3b8fe7e5657277e48ac
Ashley Porter <porter.ashley13@gmail.com>
2021-08-09 16:03:35 -0700

    add lesson 5.32 - Why Typed Arrays?

--- arrays.ts
+++ arrays.ts
@@ -5,3 +5,15 @@ const dates = [new Date(), new Date()];
 
 const carsByMake = [['f150'], ['corolla'], ['camaro']]; // Same situation
 const carsByMake2: string[][] = []; // type is double array
+
+// Help with inference when extracting values
+const carModel = carMakers[0];
+const myCarModel = carMakers.pop();
+
+// Prevent incompatible values
+// carMakers.push(100); // not allowed
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+  return car;
+});
