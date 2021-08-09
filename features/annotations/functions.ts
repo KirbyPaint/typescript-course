bf5ae16ed242ce35ba4bb0a201ae80919fe9400b
Ashley Porter <porter.ashley13@gmail.com>
2021-08-09 15:20:22 -0700

    add lesson 3.26 - Inference Around Functions

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -1,3 +1,9 @@
 const add = (a: number, b: number): number => {
   return a + b;
 };
+
+// Return annotation prevents errors such as "forgetting the return statement"
+// adding :number to the function prompts for return statement
+const subtract = (a: number, b: number) => {
+  a - b;
+}
