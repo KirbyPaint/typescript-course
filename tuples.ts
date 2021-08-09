f8958cf707cf894b6fb25adec882ae09591cbd46
Ashley Porter <porter.ashley13@gmail.com>
2021-08-09 16:27:23 -0700

    add lesson 6.37 - Why Tuples?

--- tuples.ts
+++ tuples.ts
@@ -9,3 +9,12 @@ type Drink = [string, boolean, number];
 const pepsi: Drink = ['brown', true, 40];
 const sprite: Drink = ['clear', true, 40];
 const tea: Drink = ['brown', false, 0];
+
+// No key means values are hard to understand
+const carSpecs: [number, number] = [400, 3354];
+
+// Keys make variable values instantly clear
+const carStats = {
+  horsepower: 400,
+  weight: 3354,
+};
