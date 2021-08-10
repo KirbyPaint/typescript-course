78fc47f58900ba48ef3536cec999be3f5451c3e4
Ashley Porter <porter.ashley13@gmail.com>
2021-08-09 18:10:33 -0700

    add lesson 7.49 - Fields with Inheritance

--- classes.ts
+++ classes.ts
@@ -10,6 +10,9 @@ const vehicle = new Vehicle('orange');
 console.log(vehicle.color);
 
 class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
   private drive(): void {
     console.log('vroom');
   }
@@ -20,5 +23,5 @@ class Car extends Vehicle {
   }
 }
 
-const car = new Car('blue');
+const car = new Car(4, 'blue');
 car.startDrivingProcess();
