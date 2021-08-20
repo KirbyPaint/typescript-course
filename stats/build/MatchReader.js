1082d5923da6bd110561c2efa1b3091557972878
Ashley Porter <porter.ashley13@gmail.com>
2021-08-20 16:14:22 -0700

    add lesson 11.121 - Extracting Match References - Again!

--- stats/build/MatchReader.js
+++ stats/build/MatchReader.js
@@ -8,3 +8,14 @@ var MatchReader = /** @class */ (function () {
     return MatchReader;
 }());
 exports.MatchReader = MatchReader;
+// .map((row: string[]): MatchData => {
+//   return [
+//     dateStringToDate(row[0]),
+//     row[1],
+//     row[2],
+//     parseInt(row[3]),
+//     parseInt(row[4]),
+//     row[5] as MatchResult,
+//     row[6],
+//   ];
+// });
