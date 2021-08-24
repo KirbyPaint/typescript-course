8ceacb8172173a1ba7e7f91fd246994d8dcb0237
Ash Porter <ashley.porter@eyecuelab.com>
2021-08-24 10:34:37 -0700

    add lesson 11.133 - One Last Thing

--- stats/build/index.js
+++ stats/build/index.js
@@ -2,11 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var CsvFileReader_1 = require("./CsvFileReader");
 var MatchReader_1 = require("./MatchReader");
-var WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
 var Summary_1 = require("./Summary");
-var HtmlReport_1 = require("./reportTargets/HtmlReport");
 var csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
 var matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
-var summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis("Man United"), new HtmlReport_1.HtmlReport());
+var summary = Summary_1.Summary.winsAnalysisWithHtmlReport("Man United");
 summary.buildAndPrintReport(matchReader.matches);
