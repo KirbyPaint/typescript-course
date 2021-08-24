a12bb8eeb682541c6f90cb54ba7230e3875440be
Ash Porter <ashley.porter@eyecuelab.com>
2021-08-24 12:08:33 -0700

    add lesson 12.138 - Function Generics

--- features/generics.ts
+++ features/generics.ts
@@ -23,3 +23,25 @@ class ArrayOfAnything<T> {
 }
 
 new ArrayOfAnything<string>(["a", "b", "c"]);
+
+// Example of generics with functions
+
+function printStrings(arr: string): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printNumbers(arr: numbers): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<atring>(["a", "b", "c"]);
