df3fe533ef8b3afe7b3b5a934b3267607aeb32be
Ash Porter <ashley.porter@eyecuelab.com>
2021-08-24 10:27:57 -0700

    add lesson 11.132 - Generating HTML Reports

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,17 +1,15 @@
 import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
-import { ConsoleReport } from "./reportTargets/consoleReport";
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
 import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { Summary } from "./Summary";
+import { HtmlReport } from "./reportTargets/HtmlReport";
 
 const csvFileReader = new CsvFileReader("football.csv");
 
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(
-  new WinsAnalysis("Man United"),
-  new ConsoleReport()
-);
+const summary = new Summary(new WinsAnalysis("Man United"), new HtmlReport());
 
 summary.buildAndPrintReport(matchReader.matches);
