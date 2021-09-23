133235686fe4d5a5292748a68894e43854acf717
Ash Porter <ashley.porter@eyecuelab.com>
2021-09-22 17:01:17 -0700

    add lesson 13.199 - Strict Null Checks

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,8 +1,13 @@
 import { User } from "./models/User";
-import { UserForm } from "./views/userForm";
+import { UserForm } from "./views/UserForm";
 
 const user = User.buildUser({ name: "NAME", age: 20 });
 
-const userForm = new UserForm(document.getElementById("root"), user);
+const root = document.getElementById("root");
 
-userForm.render();
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error("Root element not found");
+}
